16fbaa84f1c320cc60d8d74ad90ab38cb80b0c71
NttCh <101490631+NttCh@users.noreply.github.com>
2025-02-19 08:56:43 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 """
-bacteria_lib: A library for bacteria detection and classification.
+bacteria_lib package.
+
+This package provides modules for data loading, model building, training pipelines,
+custom callbacks, transforms, and utility functions.
 """
 
-from .data import PatchClassificationDataset
-from .models import build_classifier
-from .callbacks import PlotMetricsCallback, OptunaReportingCallback
-from .transforms import ToGray3
-from .utils import load_obj, set_seed
+__version__ = "0.1.0"
